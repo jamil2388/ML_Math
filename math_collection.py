a6635805509d8166bbd82df23c2affe543694e49
Md. Jamil Ahmed <jamil@therapservices.net>
2023-05-11 11:37:47 +0600

    Commit upto 5.1.7

--- math_collection.py
+++ math_collection.py
@@ -29,7 +29,7 @@ plt.quiver(0, 0, 3, -2, scale_units = 'xy', angles = 'xy', scale = 1, color = 'y
 plt.quiver(0, 0, -5, 25, scale_units = 'xy', angles = 'xy', scale = 1, color = 'g')
 plt.xlim(-10, 10)
 plt.ylim(-10, 10)
-plt.show()
+# plt.show()
 
 # Dot, Cross Product and Projection
 # Representing as usual 1D array vectors (not as origin(x, y) and target(x, y))
@@ -45,3 +45,30 @@ print(f'a = {a}, b = {b}, a_dot_b = {a_cross_b}')
 magnitude_b = np.sqrt(sum(b ** 2))
 a_proj_b = (a_dot_b / (magnitude_b ** 2)) * b
 print(f'a = {a}, b = {b}, magnitude_b = {magnitude_b}, a_dot_b = {a_proj_b}')
+
+# Matrix
+# Create a 2*3 Matrix
+matrix1 = np.array([[2, 3, -1], [11, 7, 9]])
+matrix2 = np.array([[2, 1, 1], [1, -5, 4], [6, -9, 2]])
+matrix3 = np.ones((5, 2), dtype = int)
+matrix4 = np.zeros((5, 5), dtype = int)
+# Notice how np.eye and np.random.rand take in the size as separate numbers instead of tuples
+matrix5 = np.eye(5, 5, dtype=  int)
+# the shape property is actually a tuple object, so it is not a callable function like 'shape()'
+print(f'matrix1 = \n{matrix1}\nShape = {matrix1.shape}')
+print(f'matrix2 = \n{matrix2}\n')
+print(f'matrix3 = \n{matrix3}\n')
+print(f'matrix4 = \n{matrix4}\n')
+print(f'matrix5 = \n{matrix5}\n')
+
+# create matrix with random values
+random_matrix = np.random.rand(3, 4)
+# the example from manual shows that the 100 here is the 'high' and the low is taken to be 0
+random_int_matrix = np.random.randint(100, size = (3, 4))
+print(f'random_matrix = \n{random_matrix}\n')
+print(f'random_int_matrix = \n{random_int_matrix}\n')
+
+# Transpose
+random_int_matrix_t = np.transpose(random_int_matrix)
+print(f'random_int_matrix_t = \n{random_int_matrix_t}\n')
+
